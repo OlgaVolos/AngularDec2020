d9cdbdef9ad457be3f96811a23b8c1b606c91610
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2021-06-08 16:04:19 +0300

    state

--- src/app/modules/comments/components/comment-details/comment-details.component.ts
+++ src/app/modules/comments/components/comment-details/comment-details.component.ts
@@ -11,20 +11,21 @@ import {IComment} from "../../models/IComment";
 export class CommentDetailsComponent implements OnInit {
   id: number;
   fullComment: IComment
-
-  constructor(private activatedRoute: ActivatedRoute, private commentService: CommentService) {
-    this.activatedRoute.params.subscribe(value => {
-      this.id = +value.id;
-      this.commentService.getComment(this.id).subscribe(comment => this.fullComment = comment)
+  constructor(private router: Router, private activatedRoute: ActivatedRoute ) {
+    this.activatedRoute.params.subscribe(params => {
+      this.fullComment = this.router.getCurrentNavigation()?.extras.state as IComment;  // console.log(history.state);
     })
+
   }
-  // constructor(private router: Router, private activatedRoute: ActivatedRoute ) {
-  //   this.activatedRoute.params.subscribe(params => {
-  //     this.fullComment = this.router.getCurrentNavigation()?.extras.state as IComment;  // console.log(history.state);
-  //   })
+
 
   // }
   ngOnInit(): void {
   }
 
 }
+// constructor(private activatedRoute: ActivatedRoute, private commentService: CommentService) {
+//   this.activatedRoute.params.subscribe(value => {
+//     this.id = +value.id;
+//     this.commentService.getComment(this.id).subscribe(comment => this.fullComment = comment)
+//   })

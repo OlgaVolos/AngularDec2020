764b116cf0b2c0a31400797072225cbd702b4464
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2021-06-08 15:10:00 +0300

    app-post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+import {PostResolveService} from "./services/post-resolve.service";
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
+let routes: Routes = [
+  {path: 'posts', component: PostsComponent, resolve: {xxx: PostResolveService}}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    PostDetailsComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
+
 
   ],
   providers: [],

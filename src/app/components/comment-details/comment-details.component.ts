621166086c9fe91346e40e6fa621b3b3564cee6f
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2021-06-08 15:38:15 +0300

    done

--- src/app/components/comment-details/comment-details.component.ts
+++ src/app/components/comment-details/comment-details.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 import {Comment} from "../../models/comment";
 import {CommentService} from "../../services/comment.service";
+
 @Component({
   selector: 'app-comment-details',
   templateUrl: './comment-details.component.html',
@@ -10,20 +11,18 @@ import {CommentService} from "../../services/comment.service";
 export class CommentDetailsComponent implements OnInit {
 
   fullComment: Comment;
+constructor(private activatedRoute: ActivatedRoute, private commentService: CommentService) {
+  this.activatedRoute.params.subscribe(value => {
+    console.log(value.id);
 
-  constructor(private activatedRoute: ActivatedRoute, private commentService: CommentService) {
-    this.activatedRoute.params.subscribe(value => {
-      console.log(value.id);
+  this.commentService.getComment(value.id).subscribe(comment => this.fullComment = comment)
+    })
 
-    this.commentService.getComment(value.id).subscribe(comment => this.fullComment = comment)
-      })}
-
+  }
   ngOnInit(): void {
 
   }
 
 }
-// constructor(private activatedRoute: ActivatedRoute, private commentService: CommentService) {
-//   this.activatedRoute.params.subscribe(value => {
-//     this.id = +value.id;
-//     this.commentService.getComment(this.id).subscribe(comment => this.fullUser = comment  )
+
+
